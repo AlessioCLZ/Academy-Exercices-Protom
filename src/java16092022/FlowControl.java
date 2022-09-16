d695932b3aa6d9f4e64f28c3541b1945340825fb
AlessioCLZ <alessio.calzetti@gmail.com>
2022-09-16 22:04:26 +0200

    excercise 2 completed

---
+++ src/java16092022/FlowControl.java
@@ -0,0 +1,60 @@
+package java16092022;
+
+import java.util.Scanner;
+
+public class FlowControl {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		
+		
+		
+		int theory, practice, minTheory=-8, maxTheory=8, minPractice=0, maxPractice=24, grade=0;
+		boolean fail= false;
+		do
+		{
+			
+			System.out.println("Inserire voto della prova teorica:");
+			theory= Integer.parseInt(keyboard.nextLine());
+			if(theory<minTheory && theory>maxTheory)
+				System.out.println("Valore errato, riprovare: è possibile inserire un valore tra -8 e +8.");
+			
+		}while(theory<minTheory && theory>maxTheory);
+		
+		do
+		{
+			System.out.println("Inserire voto della prova pratica:");
+			practice= Integer.parseInt(keyboard.nextLine());
+			if(practice<minPractice && practice>maxPractice)
+				System.out.println("Valore errato, riprovare: è possibile inserire un valore tra 0 e 24.");
+	
+		}while(practice<minPractice && practice>maxPractice);
+		
+		if(theory<=0 || (theory>0 && practice<18))
+			fail= true;
+		
+		if(!fail)
+		{
+			grade= theory+practice;
+			if(grade>30)
+			{
+				System.out.println("Congratulazioni: 30 e lode!");
+			}
+			else
+			{
+				System.out.println("Il suo voto è: " +grade+"/30");
+			}
+		}
+			
+		
+		
+		
+		
+		keyboard.close();
+	}
+
+}

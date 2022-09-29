4140fa07bee0651d17b4292793a8b2fb286e5686
AlessioCLZ <alessio.calzetti@gmail.com>
2022-09-29 11:31:08 +0200

    Library ex done

---
+++ src/java_27_09_2022/streams/library/Library.java
@@ -0,0 +1,64 @@
+package java_27_09_2022.streams.library;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Library {
+	
+	public class Book {
+
+		@SuppressWarnings("unused")
+		private String title, author;
+		private Set<String> tags;
+		
+		public Book() {}
+
+		public Book( String author, String title) {
+			super();
+			this.title = title;
+			this.author = author;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public Set<String> getTags() {
+			return tags;
+		}
+		public void setTags(Set<String> tags) {
+			this.tags = tags;
+		}
+		
+		public void addTag(String tag)
+		{
+			tags.add(tag);
+		}
+	}
+	
+	private Map<String, Book> books;
+	
+	public Library() {
+		books = new HashMap<>();
+	}
+	
+	
+	
+	public Book addBook(String author, String title)
+	{
+		Book res= new Book(author, title);	
+		books.put(res.getTitle(), res);
+		return res;
+	}
+	
+	public Set<Book> getBooksbyTag(String tag)
+	{
+		Set<Book> res = books.values().stream().filter(book -> book.getTags().contains(tag)).collect(Collectors.toSet());
+		return res;
+	}
+	
+}
